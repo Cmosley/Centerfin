bbe70b14d110fec004cc9ee61f7e46b4fcc4bd1b
cmosley <cmosley@beholdmedia.com>
2022-02-14 15:54:30 -0600

    home page background, SW template, meta data in index

---
+++ src/serviceWorker.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+// This optional code is used to register a service worker.
+// register() is not called by default.
+
+// This lets the app load faster on subsequent visits in production
+
+// To learn more about the benefits of SW 
+// https://create-react-app.dev/docs/making-a-progressive-web-app/#offline-first-considerations
+
+// extended thoughts on SW in CRA: https://jeffy.info/2018/10/10/sw-in-c-r-a.html
+
+
+// register / unregister SW 
