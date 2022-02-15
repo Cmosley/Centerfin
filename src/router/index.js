2d1bcb9374e8f4de19ac2afb02cab5a71a5bc7fe
cmosley <cmosley@beholdmedia.com>
2022-02-15 14:45:54 -0600

    status pages routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,24 @@
-const router = {
-  
-}
+import { Navigate } from "react-router-dom";
+
+import StatusLayout from ".src/pages/StatusLayout";
+import statusRoutes from "./status";
+
+const router = [
+  {
+    path: "login",
+    element: <Login />
+  },
+  {
+    path: "signup",
+    element: <Signup />
+  },
+  {
+    path: "*",
+    element: <StatusLayout />,
+    children: statusRoutes
+  }
+
+
+];
 
 export default router;
