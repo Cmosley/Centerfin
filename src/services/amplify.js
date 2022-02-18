c6876de69976ec20f8cc600c499abe1e85a3b57d
cmosley <cmosley@beholdmedia.com>
2022-02-18 17:31:28 -0600

    amplify auth ui component

---
+++ src/services/amplify.js
@@ -0,0 +1,17 @@
+// import Auth from 'aws-amplify/auth'
+
+// async function signUp() {
+//   try {
+//     const { user } = await Auth.signUp({
+//         username,
+//         password,
+//         attributes: {
+//         email, // optional
+      
+//       },
+//     });
+//     console.log(user);
+//   } catch (error) {
+//     console.log("error signing up:", error);
+//   }
+// }
